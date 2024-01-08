507dd93060b56e5c4d74992083faad990efa4749
joey rocha <jrochaperez123@gmail.com>
2024-01-08 14:43:31 -0800

    first push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import TronaldApi from './components/tronaldApi'
+
+function App() {
+  
+
+  return (
+    <>
+    <TronaldApi/>
+    </>
+  )
+}
+
+export default App
